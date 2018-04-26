d55d36e1cce2537e1c942d03c80f1b7bada33944
aluno <aluno@decom.com>
2018-04-26 12:14:49 -0300

    fim da aula

--- starwars.js
+++ starwars.js
@@ -1,5 +1,28 @@
-// Seu javascript aqui :)
-// Use a Star Wars API: https://swapi.co/
-// para carregar:
-//  - A lista de filmes
-//  - A introdução de cada filme, quando ele for clicado
+let roman= ['I','II','III','IV','V','VI','VII','VIII','IX']
+$.ajax({
+	url: "https://swapi.co/api/films/",
+	method: 'GET',      // opcional: 'GET' é o valor padrão
+	success: function(resposta) {
+		let $listElement = $('#movies > ul');
+		let list="";
+		resposta.results.sort(function(a,b){
+			return a.episode_id-b.episode_id;	
+		})
+		for(index of resposta.results){
+		    list+='<li data-episode-url="'+index.url+'">Episode '+roman[index.episode_id-1]+': '+index.title+'</li> \n';
+		}
+		$listElement.html(list);
+	}
+});
+
+$('#movies').on('click', 'li', function(e) {
+
+	$.ajax({
+		url: $(e.target).attr('data-episode-url'),
+		method: 'GET',      // opcional: 'GET' é o valor padrão
+		success: function(resposta) {
+			let $introTextEl = $('.flow > pre');
+			$introTextEl.html(resposta.opening_crawl)
+		}
+	});
+});
